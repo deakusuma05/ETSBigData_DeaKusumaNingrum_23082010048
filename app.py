75dd367c58ca23ee30ac40eb1fde3fcad290e990
Dea Kusuma Ningrum <145248502+deakusuma05@users.noreply.github.com>
2026-04-15 05:34:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
